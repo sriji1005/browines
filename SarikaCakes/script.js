0f5dd519ae092ef5c237e85661d00320e5487f05
sriji1005 <sarikasri2003@gmail.com>
2024-12-10 10:38:36 +0530

    Add files via upload

---
+++ SarikaCakes/script.js
@@ -0,0 +1,5 @@
+// JavaScript for Feedback Form Submission (optional for now)
+document.getElementById("feedback-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("Thank you for your feedback!");
+});
